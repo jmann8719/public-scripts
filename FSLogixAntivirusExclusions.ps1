3c5aa08f262290dda65dadfd8e2dda0e23c13aae
Jonathan Mann <93194236+jmann8719@users.noreply.github.com>
2024-01-07 17:35:28 +0000

    Update FSLogixAntivirusExclusions.ps1

--- FSLogixAntivirusExclusions.ps1
+++ FSLogixAntivirusExclusions.ps1
@@ -4,31 +4,24 @@ $StorageAcct = "<STORAGEACCOUNT>" # Storage Account Name
 $ShareName = "<FILESHARE>" # Storage Account's File Share Name
 
 $FileList = `
-    "%ProgramFiles%\FSLogix\Apps\frxdrv.sys", `
-    "%ProgramFiles%\FSLogix\Apps\frxdrvvt.sys", `
-    "%ProgramFiles%\FSLogix\Apps\frxccd.sys", `
-    "%TEMP%\*.VHD", `
-    "%TEMP%\*.VHDX", `
-    "%Windir%\TEMP\*.VHD", `
-    "%Windir%\TEMP\*.VHDX", `
-    "\\$StorageAcct.file.core.windows.net\$ShareName\*.VHD", `
-    "\\$StorageAcct.file.core.windows.net\$ShareName\*.VHDX"
-
-$ProcessList = `
-    "%ProgramFiles%\FSLogix\Apps\frxccd.exe", `
-    "%ProgramFiles%\FSLogix\Apps\frxccds.exe", `
-    "%ProgramFiles%\FSLogix\Apps\frxsvc.exe"
+    "%TEMP%\*\*.VHD", `
+    "%TEMP%\*\*.VHDX", `
+    "%Windir%\TEMP\*\*.VHD", `
+    "%Windir%\TEMP\*\*.VHDX", `
+    "\\$StorageAcct.file.core.windows.net\$ShareName\*\*.VHD", `
+    "\\$StorageAcct.file.core.windows.net\$ShareName\*\*.VHD.lock", `
+    "\\$StorageAcct.file.core.windows.net\$ShareName\*\*.VHD.meta", `
+    "\\$StorageAcct.file.core.windows.net\$ShareName\*\*.VHD.metadata", `
+    "\\$StorageAcct.file.core.windows.net\$ShareName\*\*.VHDX", `
+    "\\$StorageAcct.file.core.windows.net\$ShareName\*\*.VHDX.lock", `
+    "\\$StorageAcct.file.core.windows.net\$ShareName\*\*.VHDX.meta", `
+    "\\$StorageAcct.file.core.windows.net\$ShareName\*\*.VHDX.metadata"
 
 Foreach ($Item in $FileList) {
     Add-MpPreference -ExclusionPath $Item
 }
-Foreach ($Item in $ProcessList) {
-    Add-MpPreference -ExclusionProcess $Item
-}
 
 If ($CloudCache) {
-    Add-MpPreference -ExclusionPath "%ProgramData%\FSLogix\Cache\*.VHD"
-    Add-MpPreference -ExclusionPath "%ProgramData%\FSLogix\Cache\*.VHDX"
-    Add-MpPreference -ExclusionPath "%ProgramData%\FSLogix\Proxy\*.VHD"
-    Add-MpPreference -ExclusionPath "%ProgramData%\FSLogix\Proxy\*.VHDX"
+    Add-MpPreference -ExclusionPath "%ProgramData%\FSLogix\Cache\*"
+    Add-MpPreference -ExclusionPath "%ProgramData%\FSLogix\Proxy\*"
 }
