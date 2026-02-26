305f9a5bf18723cf3dccb95400bf01504937ab79
Jonathan Mann <93194236+jmann8719@users.noreply.github.com>
2026-02-26 21:13:34 +0000

    Update FSLogixAntivirusExclusions.ps1

--- FSLogixAntivirusExclusions.ps1
+++ FSLogixAntivirusExclusions.ps1
@@ -1,13 +1,54 @@
-# Windows Defender Exclusions for FSLogix
-$CloudCache = $false # Set True if using FSLogix Cloud Cache
-$StorageAcct = "<STORAGEACCOUNT>" # Storage Account Name
-$ShareName = "<FILESHARE>" # Storage Account's File Share Name
+<#
+.SYNOPSIS
+    This script creates the required Windows Defender Antivirus Exclusions for FSLogix running on Azure Virtual Desktop Session Hosts.
 
-$FileList = `
-    "%TEMP%\*\*.VHD", `
-    "%TEMP%\*\*.VHDX", `
-    "%Windir%\TEMP\*\*.VHD", `
-    "%Windir%\TEMP\*\*.VHDX", `
+.DESCRIPTION
+    This script sets up Windows Defender Antivirus exclusions as suggested by Microsoft for FSLogix.
+    The exclusions cover FSLogix executables, drivers and directories.
+    Use this only on Azure Virtual Desktop Session Hosts that run FSLogix for User Profile Containers.
+
+.EXAMPLE
+    An example of how to run the script:
+    PS C:\> .\FSLogixAntivirusExclusions.ps1
+
+.NOTES
+    Author: Jonathan Mann
+    Date: 01st March 2026
+    Version: 1.0
+
+.LINK
+    https://learn.microsoft.com/en-us/fslogix/overview-prerequisites
+#>
+
+# FSLogix Cloud Cache Exclusions
+# Update to '$True' if using Cloud Cache
+$CloudCache = $False
+
+# FSLogix Storage Account and File Share Naming
+# Specify names of the Azure Storage Account and File Share for FSLogix User Containers
+$StorageAcct = "<STORAGEACCOUNT>"
+$ShareName = "<FILESHARE>"
+
+# FSLogix Process Exclusions
+$Processes = `
+    "frxsvc.exe", `
+    "frxccds.exe"
+
+# Loop and Implement FSLogix Process Exclusions
+foreach ($Process in $Processes) {
+    Add-MpPreference -ExclusionProcess $Process
+}
+
+# FSLogix File Path Exclusions
+$Paths = `
+    "%PROGRAMFILES%\FSLogix\Apps\frxdrv.sys", `
+    "%PROGRAMFILES%\FSLogix\Apps\frxdrvvt.sys", `
+    "%PROGRAMFILES%\FSLogix\Apps\frxccd.sys", `
+    "%SYSTEMDRIVE%\Users\*\AppData\Local\FSLogix\", `
+    "%SYSTEMDRIVE%\Users\*\AppData\Local\Temp\*\*.VHD", `
+    "%SYSTEMDRIVE%\Users\*\AppData\Local\Temp\*\*.VHDX", `
+    "%WINDIR%\TEMP\*\*.VHD", `
+    "%WINDIR%\TEMP\*\*.VHDX", `
     "\\$StorageAcct.file.core.windows.net\$ShareName\*\*.VHD", `
     "\\$StorageAcct.file.core.windows.net\$ShareName\*\*.VHD.lock", `
     "\\$StorageAcct.file.core.windows.net\$ShareName\*\*.VHD.meta", `
@@ -17,11 +58,13 @@ $FileList = `
     "\\$StorageAcct.file.core.windows.net\$ShareName\*\*.VHDX.meta", `
     "\\$StorageAcct.file.core.windows.net\$ShareName\*\*.VHDX.metadata"
 
-Foreach ($Item in $FileList) {
-    Add-MpPreference -ExclusionPath $Item
+# Loop and Implement FSLogix File Path Exclusions
+foreach ($Path in $Paths) {
+    Add-MpPreference -ExclusionPath $Path
 }
 
-If ($CloudCache) {
-    Add-MpPreference -ExclusionPath "%ProgramData%\FSLogix\Cache\*"
-    Add-MpPreference -ExclusionPath "%ProgramData%\FSLogix\Proxy\*"
+# If utilised, FSLogix Cloud Cache Exclusions.
+if ($CloudCache) {
+    Add-MpPreference -ExclusionPath "%PROGRAMDATA%\FSLogix\Cache\*"
+    Add-MpPreference -ExclusionPath "%PROGRAMDATA%\FSLogix\Proxy\*"
 }
